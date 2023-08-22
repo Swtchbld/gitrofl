ad59656c5f538fda51254e1c9cde6525b0711a85
Alex <s.vadyuk2015@gmail.com>
2023-08-22 13:56:35 +0300

    part 3

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-const numberOfFilms = +prompt('How many films have you seen arleady?', '');
+/* eslint-disable indent */
+let numberOfFilms ;
+function start(){
+     numberOfFilms = +prompt('How many films have you seen arleady?', '');
+    while(numberOfFilms==''|| numberOfFilms ==null||isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('How many films have you seen arleady?', '');
+    }
+}
+start();
 
 const personalMovieDB =  {
 	count: numberOfFilms,
@@ -8,11 +16,50 @@ const personalMovieDB =  {
 	private: false
 };
 
-const a =prompt ('What film you have watched last?',''),
-	b= prompt ('how you can rate it?',''),
-	c= prompt('What film you have watched last?',''),
-	d= prompt ('how you can rate it?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function rememberMyFilms(){
+    for (let i=0; i<2; i++  ){
+        const a =prompt ('What film you have watched last?',''),
+        b= prompt ('how you can rate it?','');
+    
+        if (a != null && b!= null && a !='' && b!='' &&a.length<50){
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else{
+            console.log('error');
+            i--;
+        }
+}
+}
+rememberMyFilms();
+
+function detectedPersonaleLevel(){
+    if (personalMovieDB.count <10 ){
+        console.log('you are gay'); 
+    } else if(personalMovieDB.count>=10 && personalMovieDB.count<30){
+        console.log('you are gaylord');
+    } else if (personalMovieDB.count >=30){
+        console.log('you are fucking pidar');
+    } else {
+        console.log('errorrrr');
+    }
+    
+    
+}
+detectedPersonaleLevel();
+
+function showMyDB(hidden){
+    if(!hidden){
 console.log(personalMovieDB);
+
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i=1; i<=3; i++){
+    
+        personalMovieDB.genres[i-1] = prompt(`What is yours favorite genre?${i}`);
+    }
+}
+writeYourGenres();
