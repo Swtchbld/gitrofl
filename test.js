f31a91489fbda11a74aae80713445c10b0270022
Alex <s.vadyuk2015@gmail.com>
2023-08-23 12:35:53 +0300

    idk

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 /* eslint-disable indent */
-let numberOfFilms ;
+ let numberOfFilms ;
 function start(){
      numberOfFilms = +prompt('How many films have you seen arleady?', '');
     while(numberOfFilms==''|| numberOfFilms ==null||isNaN(numberOfFilms)){
@@ -17,49 +17,66 @@ const personalMovieDB =  {
 };
 
 
-function rememberMyFilms(){
-    for (let i=0; i<2; i++  ){
-        const a =prompt ('What film you have watched last?',''),
-        b= prompt ('how you can rate it?','');
-    
-        if (a != null && b!= null && a !='' && b!='' &&a.length<50){
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('What film you have watched last?', '').trim(),
+            b = prompt('how you can rate it?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
             console.log('done');
-        } else{
+        } else {
             console.log('error');
             i--;
         }
-}
+    }
 }
 rememberMyFilms();
 
-function detectedPersonaleLevel(){
-    if (personalMovieDB.count <10 ){
-        console.log('you are gay'); 
-    } else if(personalMovieDB.count>=10 && personalMovieDB.count<30){
+function detectedPersonaleLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('you are gay');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
         console.log('you are gaylord');
-    } else if (personalMovieDB.count >=30){
+    } else if (personalMovieDB.count >= 30) {
         console.log('you are fucking pidar');
     } else {
         console.log('errorrrr');
     }
-    
-    
+
+
 }
 detectedPersonaleLevel();
 
-function showMyDB(hidden){
-    if(!hidden){
-console.log(personalMovieDB);
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
 
     }
 }
 showMyDB(personalMovieDB.privat);
 
 function writeYourGenres() {
-    for (let i=1; i<=3; i++){
-    
-        personalMovieDB.genres[i-1] = prompt(`What is yours favorite genre?${i}`);
+    for (let i = 1; i <= 3; i++) {
+
+        personalMovieDB.genres[i - 1] = prompt(`What is yours favorite genre?${i}`);
     }
 }
-writeYourGenres();
+writeYourGenres(); 
+ 
+ /*     let num;
+    let volume;
+    let square;
+    function calculateVolumeAndArea() {
+        num=+prompt('put cube length','');
+        volume=num*num*num;
+        square =(num*num)*6;
+        if(volume==0 || volume<0||square==0||square<0){
+            console.log('this is error');
+        } else  console.log('this your volume of cube',volume,'this is cube square',square);
+       
+    }
+    calculateVolumeAndArea(num); 
+
+     */
+
